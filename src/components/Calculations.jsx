35b2a33df8bc2bbb24c42b171a76f158955da8d3
10-menachi <bobitlmr188221@spu.ac.ke>
2023-10-19 11:56:54 +0300

    Implement the layout and markup of the application

---
+++ src/components/Calculations.jsx
@@ -0,0 +1,7 @@
+const Calculations = () => (
+  <div className="pallete">
+    <h2>0</h2>
+  </div>
+);
+
+export default Calculations;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import Calculations from './Calculations';
+import Keys from './Keys';
+
+const Calculator = () => (
+  <div className="calculator">
+    <Calculations />
+    <div className="keys">
+      <Keys />
+    </div>
+  </div>
+);
+
+export default Calculator;
